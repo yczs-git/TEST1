92c6fc31d43fb4ce9e77ec5b8ffb7fdd9dba044e
wangqiaoyun <1627603819@qq.com>
2021-03-03 17:08:10 +0800

    测试自动化部署

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
   <p>java0821全体同学 前程似锦加油呀</p>
   <p>java0821身体健康 快快乐乐</p>
-  <p>java0821财源滚滚哟拿到offer</p>
+  <p>java0821财源滚滚哟拿到offer拉拉</p>
   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
